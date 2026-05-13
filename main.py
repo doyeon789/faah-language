b4edb06f2589d4cde52d8e8d611e24b4fb1e78db
doyeon789 <ldyeon789@naver.com>
2026-05-13 15:24:56 +0900

    feat: runfile 생성

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 import sys
 
+def run_file(path: str):
+    pass
+
+
 if __name__ == '__main__':
     args = sys.args[1:]
-    
+
+    # -- <= 콘솔 명령어 나중에 만들기 위해서
+    # -- 제외한 파일 불러오기
+    files= [a for a in args if not a.startswith('--')]
+
+    run_file(files)
