19ee04539c62497696c58230d69f63dfe43ca211
doyeon789 <ldyeon789@naver.com>
2026-05-13 16:50:24 +0900

    feat: 문장 실행 코드 작성, 식 평가 코드 작성

--- fah/interpreter.py
+++ fah/interpreter.py
@@ -20,6 +20,26 @@ class Interpreter:
     def set_var(self, index: int, value: int):
         self.variables[index] = value
 
+    # 실행
     def run(self, program: Program):
         for stmt in program.body:
             self.exec_stmt(stmt)
+
+    # 문장 실행
+    def exec_stmt(self, node):
+        if isinstance(node, VarSet):
+            value = self.eval_expr(node.expr)
+            self.set_var(node.index, value)
+
+        else:
+            raise RuntimeError_(f"알 수 없는 노드: {type(node)}")
+        
+    # 식 평
+    def eval_expr(self, node) -> int:
+        if isinstance(node, IntLiteral):
+            return node.value
+
+        if isinstance(node, VarGet):
+            return self.get_var(node.index)
+
+        raise RuntimeError_(f"알 수 없는 식 노드: {type(node)}")
